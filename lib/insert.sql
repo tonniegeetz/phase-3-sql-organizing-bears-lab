c3338933404d86f84a468b03c374eba4ec5fa4c2
tonniegeetz <tonniegeetz@gmail.com>
2024-06-26 12:20:14 +0300

    SQL queries

---
+++ lib/insert.sql
@@ -0,0 +1,20 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Mr. Chocolate',25,'M','Chocolate','Average', True);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Rowdy',21,'M','Black','High', True);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Tabitha',32,'F','Brown','Average', True);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Sergeant Brown',35,'M','White','Low', True);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Melissa',25,'F','Dark','Average', True);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Grinch',28,'M','Green','Average', True);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Wendy',25,'F','Chocolate','Average', True);
+
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(NULL,25,'M','Chocolate','Average', True);
+
+
+
+
+
